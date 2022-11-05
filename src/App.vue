0c0cf1a39915b25799d78a20c79f63bfd61fd534
Дмитрий <105929374+Dimon1071@users.noreply.github.com>
2022-11-05 18:17:22 +0200

    Декомпозиция, обмен данными между компонентами

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,18 @@
 <template>
   <div class="app">
-    <form @submit.prevent>
-      <h4>Создание поста</h4>
-      <input
-       @input="title = $event.target.value" 
-       :value="title" 
-       class="input" 
-       type="text" 
-       placeholder="Название">
-
-      <input
-      @input="body = $event.target.value" 
-      :value="body" 
-      class="input" 
-      type="text" 
-      placeholder="Описание">
-
-      <button @click="addPost">Создать</button>
-    </form>
-
-    <div class="post" v-for="post in posts">
-      <div><strong>Название: </strong>{{ post.title }}</div>
-      <div><strong>Описаниие: </strong>{{ post.body }}</div>
-    </div>
+    <post-form @create="createPost" />
+    <post-list :posts="posts" />
   </div>
 </template>
 
 <script>
+ import PostForm from "@/components/PostForm.vue";
+ import PostList from "@/components/PostList.vue"; 
 
   export default {
+    components: {
+     PostForm, PostList
+    },
     data(){
       return {
         posts: [
@@ -36,20 +20,11 @@
           {id: 2, title: 'JavaScript 2', body: 'описание поста 2'},
           {id: 3, title: 'JavaScript 3', body: 'описание поста 3'},
         ],
-        title: '',
-        body: '',
       }
     },
     methods: {
-      addPost() {
-        const newPost = {
-          id: Date.now(),
-          title: this.title,
-          body: this.body,
-        }
-        this.posts.push(newPost);
-        this.title = '';
-        this.body = ''; 
+      createPost(post) {
+         this.posts.push(post)
       }
     }
   }
@@ -62,19 +37,6 @@
   padding: 0;
   box-sizing: border-box;
  }
-
-.post {
-  padding: 15px;
-  border: 3px solid teal; 
-  margin-top: 15px;
-}
-
-.input {
-  width: 100%;
-  border: 1px solid teal;
-  padding: 10px 15px;
-  margin-top: 15px;
-}
 
 .app {
   padding: 15px;
---
+++ src/components/PostList.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h3>Список пользователей</h3>
+        <post-item :post="post" v-for="post in posts" />
+    </div>
+        
+</template>
+
+<script>
+ import PostItem from "./PostItem.vue";
+ export default {
+    components: {PostItem},
+    props: {
+       posts: {
+         type: Array,
+         required: true,
+       }
+    }
+ }
+</script>
+
+<style scoped>
+
+    .post {
+        padding: 15px;
+        border: 3px solid teal; 
+        margin-top: 15px;
+    }
+
+</style>
