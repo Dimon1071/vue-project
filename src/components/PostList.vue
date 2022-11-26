4a7dcbca2721e9e5449642870c2fbe42189e172b
Дмитрий <105929374+Dimon1071@users.noreply.github.com>
2022-11-26 22:34:01 +0200

    Анимация

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,13 @@
 <template>
     <div>
       <h3>Список постов:</h3>
+      <transition-group name="user-list">
         <post-item 
           :post="post" v-for="post in posts" 
           :key="post.id" 
           @remove="$emit('remove', post)" />
+      </transition-group>
     </div>
-        
 </template>
 
 <script>
@@ -23,6 +24,23 @@
 </script>
 
 <style>
+  .user-list-item {
+    display: inline-block;
+    margin-right: 10px;
+  }
 
-    
+  .user-list-enter-active,
+  .user-list-leave-active {
+    transition: all 0.4s ease;
+  }
+  
+  .user-list-enter-form,
+  .user-list-leave-to {
+    opacity: 0;
+    transform: translateX(130px);
+  }
+
+  .user-list-move {
+    transition: transform 0.8s ease;
+  }
 </style>
